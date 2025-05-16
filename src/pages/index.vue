1dd1b5214f7a19ce89d1e8e61dc32ea13d2f8268
SadxDarkness <60146426+SadxDarkness@users.noreply.github.com>
2025-05-16 20:30:58 +0300

    хидер

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,36 @@
 <template>
   <main>
     <section>
+      <base-header></base-header>
+    </section>
+
+    <section>
       <input-file></input-file>
       <configure-art></configure-art>
     </section>
+
     <section>
       <show-image></show-image>
       <show-art></show-art>
     </section>
+
     <section>
       <generate-art></generate-art>
     </section>
+
   </main>
 </template>
 <script setup>
+
+import BaseHeader from '@/components/BaseHeader'
+import GenerateArt from '@/components/GenerateArt'
+import InputFile from '@/components/InputFile'
+import ShowArt from '@/components/ShowArt'
+import ShowImage from '@/components/ShowImage'
+import ConfigureArt from '@/components/ConfigureArt'
+
+
 defineOptions({ name: 'MainFile'})
-import GenerateArt from '../components/GenerateArt/index.vue'
-import InputFile from '../components/InputFile/index.vue'
-import ShowArt from '../components/ShowArt/index.vue'
-import ShowImage from '../components/ShowImage/index.vue'
-import ConfigureArt from '../components/ConfigureArt/index.vue'
 
 </script>
 
